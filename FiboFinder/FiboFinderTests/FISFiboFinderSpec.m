ceb7e7475078756aa5971ca27f0e5117baf2a3ef
Chris Gonzales <gonzales.chris.a@gmail.com>
2014-06-02 22:34:09 -0400

    add tests

--- FiboFinder/FiboFinderTests/FISFiboFinderSpec.m
+++ FiboFinder/FiboFinderTests/FISFiboFinderSpec.m
@@ -16,9 +16,31 @@
 SpecBegin(FISFiboFinder)
 
 describe(@"FISFiboFinder", ^{
-    
+    __block FISFiboFinder *fiboOjb;
     beforeAll(^{
-
+        fiboOjb = [[FISFiboFinder alloc] init];
+    });
+    
+    describe(@"findFibo", ^{
+        it(@"should return the nth number of the Fibonacci sequence",^{
+            expect([fiboOjb findFiboNumber:0]).to.equal(0);
+        });
+        
+        it(@"should return the nth number of the Fibonacci sequence",^{
+            expect([fiboOjb findFiboNumber:1]).to.equal(1);
+        });
+        
+        it(@"should return the nth number of the Fibonacci sequence",^{
+            expect([fiboOjb findFiboNumber:4]).to.equal(3);
+        });
+        
+        it(@"should return the nth number of the Fibonacci sequence",^{
+            expect([fiboOjb findFiboNumber:7]).to.equal(13);
+        });
+        
+        it(@"should return the nth number of the Fibonacci sequence",^{
+            expect([fiboOjb findFiboNumber:10]).to.equal(55);
+        });
     });
     
 });
