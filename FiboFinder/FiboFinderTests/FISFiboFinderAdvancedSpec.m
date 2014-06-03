f66b782628dec87eee6c2c0f1173cb1d0de57699
Chris Gonzales <gonzales.chris.a@gmail.com>
2014-06-03 16:45:23 -0400

    update AdvancedSpec

--- FiboFinder/FiboFinderTests/FISFiboFinderAdvancedSpec.m
+++ FiboFinder/FiboFinderTests/FISFiboFinderAdvancedSpec.m
@@ -1,38 +1,50 @@
 //
-//  FISFiboFinderAdvancedSpec.m
+//  FISFiboFinderAdvancedSpecSpec.m
 //  FiboFinder
 //
 //  Created by Chris Gonzales on 6/3/14.
 //  Copyright 2014 FIS. All rights reserved.
 //
 
+#ifdef ADVANCED
 #import "Specta.h"
 #import "FISFiboFinderAdvanced.h"
-
+#define EXP_SHORTHAND
+#import "Expecta.h"
+#import "KIF.h"
+#import "Swizzlean.h"
 
 SpecBegin(FISFiboFinderAdvanced)
 
 describe(@"FISFiboFinderAdvanced", ^{
-    
+    __block FISFiboFinderAdvanced *fiboOjb;
     beforeAll(^{
-
+        fiboOjb = [[FISFiboFinderAdvanced alloc] init];
     });
     
-    beforeEach(^{
-
+    describe(@"findFibo", ^{
+        it(@"should return the nth number of the Fibonacci sequence",^{
+            expect([fiboOjb findFiboNumber:0]).to.equal(0);
+        });
+        
+        it(@"should return the nth number of the Fibonacci sequence",^{
+            expect([fiboOjb findFiboNumber:1]).to.equal(1);
+        });
+        
+        it(@"should return the nth number of the Fibonacci sequence",^{
+            expect([fiboOjb findFiboNumber:4]).to.equal(3);
+        });
+        
+        it(@"should return the nth number of the Fibonacci sequence",^{
+            expect([fiboOjb findFiboNumber:7]).to.equal(13);
+        });
+        
+        it(@"should return the nth number of the Fibonacci sequence",^{
+            expect([fiboOjb findFiboNumber:10]).to.equal(55);
+        });
     });
     
-    it(@"", ^{
-
-    });  
-    
-    afterEach(^{
-
-    });
-    
-    afterAll(^{
-
-    });
 });
 
 SpecEnd
+#endif
