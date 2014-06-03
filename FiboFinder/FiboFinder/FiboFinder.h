7c73e4d34c13e670bd9dfb6637607225a7aae758
Chris Gonzales <gonzales.chris.a@gmail.com>
2014-06-02 22:23:58 -0400

    add FISFiboFinder files

---
+++ FiboFinder/FiboFinder/FiboFinder.h
@@ -0,0 +1,15 @@
+//
+//  FiboFinder.h
+//  FiboFinder
+//
+//  Created by Chris Gonzales on 6/2/14.
+//  Copyright (c) 2014 FIS. All rights reserved.
+//
+
+#import <Foundation/Foundation.h>
+
+@interface FiboFinder : NSObject
+
+//declare methods here
+
+@end
---
+++ FiboFinder/FiboFinder/FiboFinder.m
@@ -0,0 +1,16 @@
+//
+//  FiboFinder.m
+//  FiboFinder
+//
+//  Created by Chris Gonzales on 6/2/14.
+//  Copyright (c) 2014 FIS. All rights reserved.
+//
+
+#import "FiboFinder.h"
+
+@implementation FiboFinder
+
+//define methods here
+
+
+@end
