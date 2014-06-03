08fd9b3c5ff684c6832928403f84fc037e058a8e
Chris Gonzales <gonzales.chris.a@gmail.com>
2014-06-03 16:44:15 -0400

    add FiboFinderAdvancedSpec file to master

---
+++ FiboFinder/FiboFinderTests/FISFiboFinderAdvancedSpec.m
@@ -0,0 +1,38 @@
+//
+//  FISFiboFinderAdvancedSpec.m
+//  FiboFinder
+//
+//  Created by Chris Gonzales on 6/3/14.
+//  Copyright 2014 FIS. All rights reserved.
+//
+
+#import "Specta.h"
+#import "FISFiboFinderAdvanced.h"
+
+
+SpecBegin(FISFiboFinderAdvanced)
+
+describe(@"FISFiboFinderAdvanced", ^{
+    
+    beforeAll(^{
+
+    });
+    
+    beforeEach(^{
+
+    });
+    
+    it(@"", ^{
+
+    });  
+    
+    afterEach(^{
+
+    });
+    
+    afterAll(^{
+
+    });
+});
+
+SpecEnd
