2b720d7a0e85a687a6a2329ada071ff5a52a8128
Chris Gonzales <gonzales.chris.a@gmail.com>
2014-06-02 22:26:32 -0400

    add FiboFinderSpec

---
+++ FiboFinder/FiboFinderTests/FISFiboFinderSpec.m
@@ -0,0 +1,26 @@
+//
+//  FISFiboFinderSpec.m
+//  FiboFinder
+//
+//  Created by Chris Gonzales on 6/2/14.
+//  Copyright 2014 FIS. All rights reserved.
+//
+
+#import "Specta.h"
+#import "FISFiboFinder.h"
+#define EXP_SHORTHAND
+#import "Expecta.h"
+#import "KIF.h"
+#import "Swizzlean.h"
+
+SpecBegin(FISFiboFinder)
+
+describe(@"FISFiboFinder", ^{
+    
+    beforeAll(^{
+
+    });
+    
+});
+
+SpecEnd
